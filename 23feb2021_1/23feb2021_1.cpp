b4be338eeda4fadf4ca045c777b4be3ad3ba5c88
onivia <ohnivia@gmail.com>
2021-02-25 19:50:51 -0500

    Commit inicial by onivia

---
+++ 23feb2021_1/23feb2021_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//a[2][3][2]
+//a[2][3][3][2]
+
+int main() {
+    int a[2][3]= {
+
+        {10,20,30}, //posicion 0 de la 1°D
+
+        {40,50,60} //posicion 1 de la 1°D
+    };
+
+    cout<<a[1][1]<<endl;    
+
+    return 0;
+}
