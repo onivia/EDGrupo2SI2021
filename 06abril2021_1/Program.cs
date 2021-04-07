56c19d33ad37b7bd9be3692bebae79f029855f48
onivia <ohnivia@gmail.com>
2021-04-06 20:57:04 -0500

    tipos de datos C#

---
+++ 06abril2021_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06abril2021_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Numericos:
+                byte    (8b)
+                short   (16b)
+                int     (32b)
+                long    (64b) 
+                -----
+                float   (32b)
+                double  (64b)
+                decimal (128b)
+            Caracter:
+                char    (16b)
+            Logico:
+                bool    (8b)
+            */
+            byte numerobyte = byte.MaxValue;
+            short numeroshort = short.MaxValue;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
