afeb02bb4c1d01772070d033f90430a25658548f
onivia <ohnivia@gmail.com>
2021-04-27 19:10:56 -0500

    Arbol, implementacion Insertar y Obtener.

--- 22abril2021_1/ArbolBinBusqueda.cs
+++ 22abril2021_1/ArbolBinBusqueda.cs
@@ -29,9 +29,79 @@ namespace _22abril2021_1
                 nodoAux = raiz;
                 while(true) {
                     //TODO: Complete aqui
-
+                    /*
+                    While:
+                        *Si 'nodo.llave'<'nA.llave':
+                            *Si 'nA.izq'!=null:
+                                nA = nA.izq
+                            *SiNo
+                                nA.izq = nodo
+                                break
+                        *SiNo 'nodo.llave'>'nA.llave':
+                            *Si 'nA.der'!=null:
+                                nA = nA.der
+                            *SiNo
+                                nA.der = nodo
+                                break
+                        *SiNo
+                            lanzar excepcion, ya existe esa llave
+                    */
+                    if(nodo.llave < nodoAux.llave) {
+                        if(nodoAux.izq!=null)
+                            nodoAux = nodoAux.izq;
+                        else {
+                            nodoAux.izq = nodo;
+                            break;
+                        }
+                    }
+                    else if(nodo.llave > nodoAux.llave) {
+                        if(nodoAux.der!=null)
+                            nodoAux = nodoAux.der;
+                        else {
+                            nodoAux.der = nodo;
+                            break;
+                        }
+                    }
+                    else { //ya existe la llave
+                        throw new Exception($"la llave: {nodo.llave} ya existe, no se permiten llaves repetidas.");
+                    }
                 }
             }
+        }
+
+        public string Obtener(int llave) {
+            /*
+            1. Declarar 'datoHallado' para retornar el dato
+            2. Si el arbol 'no este vacio':
+                3. posicionar a 'nA' en 'raiz'
+                4. Mientras un cliclo infinito:
+                    5. Si 'nA' es null rompemos el ciclo 'break'
+                    6. Si 'llave'=='nA.llave', asigno 'datoHallado' con 'nA.dato' y rompo el ciclo
+                    7. Si 'llave'<'nA.llave', muevo a 'nA' a su izq
+                    8. Si 'llave'>'nA.llave', muevo a 'nA' a su der
+                9. retorno a 'datoHallado'
+            */
+            string datoHallado = null;
+            Nodo nodoAux = null;
+
+            if(!estaVacio()) {
+                nodoAux = raiz;
+                while(true) {
+                    if(nodoAux==null)
+                        break;
+                    
+                    if(llave==nodoAux.llave) {
+                        datoHallado = nodoAux.dato;
+                        break;
+                    }
+                    else if(llave < nodoAux.llave)
+                        nodoAux = nodoAux.izq;
+                    else if(llave > nodoAux.llave)
+                        nodoAux = nodoAux.der;
+                }                
+            }
+
+            return datoHallado;
         }
     }
 }
--- 22abril2021_1/Program.cs
+++ 22abril2021_1/Program.cs
@@ -11,7 +11,16 @@ namespace _22abril2021_1
 
         public static void Prueba_ArbolBinarioBusqueda() {
             //TODO: haga aqui la prueba de Insercion en una instancia de tipo 'ArbolBinarioBusqueda'
+            ArbolBinBusqueda arbol = new ArbolBinBusqueda();
+            string dato = null;
 
+            arbol.Insertar(6,"seis");
+            arbol.Insertar(3,"tres");
+            arbol.Insertar(9,"nueve");
+            arbol.Insertar(2,"dos");
+            arbol.Insertar(10,"diez");
+            dato = arbol.Obtener(10);
+            Console.WriteLine($"el dato es: {dato}");
         }
     }
 }
